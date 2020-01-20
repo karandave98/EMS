2139b9f0deb72f335601054a88b68d08a606bdea
karandave98 <dkaran141@gmail.com>
2020-01-20 11:30:19 +0530

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+import { EmployeesComponent } from './employees/employees.component';
+
+
+const appRoutes: Routes = [
+  { path: '', component: AppComponent },
+  { path: '\employees', component: EmployeesComponent },
+];
+
+
+@NgModule({
+  imports:      [ BrowserModule, FormsModule, RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ) ],
+  
+  declarations: [ AppComponent, HelloComponent , EmployeesComponent],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
---
+++ src/app/employees/employees.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-employees',
+  templateUrl: './employees.component.html',
+  styleUrls: ['./employees.component.css']
+})
+export class EmployeesComponent implements OnInit {
+
+  constructor() { }
+  employees = [ 
+        { 
+          id: 1, 
+          name: "Ram", 
+          location: "Bangalore", 
+          email: "ram@mail.com", 
+          mobile: "9867512345" 
+        }, 
+        { 
+          id: 2, 
+          name: "Raj", 
+          location: "Chennai", 
+          email: "raj@mail.com", 
+          mobile: "7867534521" 
+        }, 
+        { 
+          id: 3, 
+          name: "Vinay", 
+          location: "Pune", 
+          email: "vinay@mail.com", 
+          mobile: "9975287450" 
+        } 
+      ]; 
+  ngOnInit() {
+  }
+
+}
