7aba088ed4fc84163bfbd0c8592a808ae39e6110
karandave98 <dkaran141@gmail.com>
2020-01-23 17:21:00 +0530

    3 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,12 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { EmployeesComponent } from './employees/employees.component';
 import { FilterPipe } from './pipes/filter.pipe';
+import { AddEmployeeComponent } from './add-employee/add-employee.component';
 
 const appRoutes: Routes = [
   
-  { path: '\employees', component: EmployeesComponent }
+  { path: '\employees', component: EmployeesComponent },
+  { path: '\addEmployee', component : AddEmployeeComponent}
 ];
 
 
@@ -19,7 +21,7 @@ const appRoutes: Routes = [
       appRoutes // <-- debugging purposes only
     ) ],
   
-  declarations: [ AppComponent, HelloComponent , EmployeesComponent , FilterPipe],
+  declarations: [ AppComponent, HelloComponent , EmployeesComponent , FilterPipe, AddEmployeeComponent],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
