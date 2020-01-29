937358ceb15110ad36d80806417895be0ca143a4
karandave98 <dkaran141@gmail.com>
2020-01-29 19:10:31 +0530

    1 file modified

--- src/app/employees/employees.component.ts
+++ src/app/employees/employees.component.ts
@@ -12,49 +12,17 @@ export class EmployeesComponent implements OnInit {
   constructor(private dataServe : DataService , private router: Router) { }
   empCount : Number = 0;
   employees = [ ]
-  //       { 
-  //         id: 1, 
-  //         name: "Ram", 
-  //         location: "Bangalore", 
-  //         email: "ram@mail.com", 
-  //         mobile: "9867512345" 
-  //       }, 
-  //       { 
-  //         id: 2, 
-  //         name: "Raj", 
-  //         location: "Chennai", 
-  //         email: "raj@mail.com", 
-  //         mobile: "7867534521" 
-  //       }, 
-  //       { 
-  //         id: 3, 
-  //         name: "Vinay", 
-  //         location: "Pune", 
-  //         email: "vinay@mail.com", 
-  //         mobile: "9975287450" 
-  //       }
-  //     ]; 
     
   ngOnInit() {
     this.employees = this.dataServe.getData();
     this.empCount = this.employees.length;
-    // if(this.dataServe.getData() != null){
-    //      let emp  = this.dataServe.getData();
-      
-    // }
-    // else{
-    //   console.log("Enter Else");
-     
-    // }
-     this.empCount = this.employees.length;
+   
   }
   clicked(ind){
     console.log(ind+1);
     this.dataServe.setEditData(ind+1);
   }
   delete(ind){
-   
-    
     for(let emp of this.employees){
       if(parseInt(emp['id'])== (ind+1)){
         this.employees.splice(ind,1);
@@ -66,5 +34,6 @@ export class EmployeesComponent implements OnInit {
         emp['id'] = emp['id']-1; 
       }
     }
+      this.empCount = this.employees.length;
   }
 }
